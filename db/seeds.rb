790f5ebec9b2be6405724253775d584a9986b51c
Mahendra Choudhary <mahendrachoudhary1156@gmail.com>
2019-11-01 10:46:37 +0530

    added todos controller for api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Todo.create(title: "Buy food: milk, bread, fruits", done: false)
+Todo.create(title: "Imagine Dragons tickets", done: false)
