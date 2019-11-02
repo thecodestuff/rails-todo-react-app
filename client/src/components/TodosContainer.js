4b66395eb37227b84911f9aca7a51f3d5a6b1788
mahendra choudhary <mahendrachoudhary1156@gmail.com>
2019-11-02 23:16:42 +0530

    added create todo

--- client/src/components/TodosContainer.js
+++ client/src/components/TodosContainer.js
@@ -8,27 +8,60 @@ class TodosContainer extends Component{
     this.state = {
       isLoaded: false,
       error: null,
+      inputValue: '',
       todos:[]
     }
+  }
+
+  handleChange = event => {
+    const {value} = event.target
+    this.setState({
+      inputValue:value 
+    })
   }
 
   componentDidMount = () => {
     this.getTodos();
   }
   
-  // Get list of todos from backend
+  // Get list of todos from /api/v1/todos endpoint
   getTodos = () => {
     axios.get('/api/v1/todos').then( response => {
       this.setState({
         isLoaded:true,
         todos: response.data
       })
+      console.log(this.state.todos)
     }).catch( error => {
       this.setState({
-        isLoaded: false,
+        isLoaded: true,
         error
       })
+      console.log(this.state.error)
     })
+  }
+
+  // create a new todo when enter 
+  createTodo = (event) => {
+    // call api POST /api/v1/todos when enter is press
+    if(event.key === 'Enter'){
+      axios.post('api/v1/todos', {todo: {title: event.target.value}}).then(response => {
+        const todo = response.data
+        console.log(this.state.todos)
+        //Updating state with current todo
+        this.setState({
+          todos: this.state.todos.concat(todo),
+          inputValue: ''
+        })
+        console.log(this.state.todos)
+      }).catch(error => {
+        this.setState({
+          isLoaded: true,
+          error
+        })
+        console.log("error while creating new task")
+      })
+    }
   }
 
 	render(){
@@ -37,8 +70,12 @@ class TodosContainer extends Component{
 			<div>
         <div className="inputContainer">
           <input className="taskInput"
-            type="text" placeholder="Add a task" maxLength="50" onChange={this.handleChange}/>
-          <button onClick={this.submitForm}>Add</button>
+            type="text" placeholder="Add a task" 
+            maxLength="50" 
+            onKeyPress={this.createTodo}  
+            onChange={this.handleChange}
+          />
+          
         </div>
         <div className="listWrapper">
           <ul className="taskList">
