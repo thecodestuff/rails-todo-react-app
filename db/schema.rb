9ddf86f1dfdec8e737226398bbca35c2d2302e36
Mahendra Choudhary <mahendrachoudhary1156@gmail.com>
2019-11-26 10:43:56 +0530

    added user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_01_045724) do
+ActiveRecord::Schema.define(version: 2019_11_26_050734) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.boolean "done"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
