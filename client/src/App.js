d1acce36dd22cb9f9bdb105d2cdfdb9be16db7b4
Mahendra Choudhary <mahendrachoudhary1156@gmail.com>
2019-12-12 11:04:58 +0530

    Added react routing basic

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,34 @@
 import React, {Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import Login from './components/LoginComponent'
 import './App.css';
-//Importing components 
+//Importing components
 import TodosContainer from './containers/TodosContainer.js'
 
 class App extends Component{
   render(){
     return (
       <div className="container">
+      <div>
+       <Router>
+         <div>
+         <ul>
+           <li><Link to="/login">Login</Link></li>
+           <li><Link to="/logout">Sign Up</Link></li>
+         </ul>
+         </div>
+
+         <hr/>
+         <Switch>
+           <Route path="/login"><Login/></Route>
+         </Switch>
+        </Router>
+      </div>
         <div className="header">
           <h1>Todo List</h1>
         </div>
